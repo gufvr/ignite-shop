4c41f1db9c737cd4bbc17d802384aee4bc01faa3
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-08-18 00:33:34 -0300

    installing and testing the Stitches lib

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257E6',
+    }
+  }
+})
