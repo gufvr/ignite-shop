5f49c256623f93dbb30cc3ba6fe1096ff82753d9
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-08-22 23:30:22 -0300

    setting the application's global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -11,7 +11,15 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257E6',
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#C4C4CC',
+      gray100: '#E1E1E6',
+
+      green500: '#00875F',
+      green300: '#00B37E'
     }
   }
 })
